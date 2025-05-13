b4b4f9871e64024fed17b145adc18a5c10a75fd4
SubFabula <150776952+SubFabula@users.noreply.github.com>
2025-05-13 19:59:11 +0300

    Update28 Fix16

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -1,35 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const sidebar = document.getElementsByClassName("sidebar");
-  const sidebarButton = document.getElementById("sidebar-button");
+  const sidebar = document.querySelector(".sidebar");
   const sidebarROTC = document.getElementById("rest-of-the-content"); // sidebarROTC => sidebarRestOfTheContent
+  const sidebarRealButton = document.getElementById("sidebarRealButton");
+  const sidebarButtonImg = document.querySelector(".sidebar-buttonCLASS"); // query selector if styling a class (something to do with «HTMLclass» and «single elements» stuff)
 
-  sidebarButton.addEventListener("click", sidebarOpenClose);
+  sidebarRealButton.addEventListener("click", () => {
+    const sidebarClosed = document.getElementById("menu-button");
+    const sidebarOpen = document.getElementById("back-icon");
+
+    if (sidebarClosed != null) {
+      sidebarROTC.style.display = `none`;
+      sidebarClosed.id = "back-icon"; 
+      sidebarClosed.style.display = `inherit`;
+      //sidebarOpen.style.display = `none`;
+      sidebar.style.width = `50px`;
+      sidebar.style.padding = `20px 20px 20px 20px`;
+      sidebar.style.background = `#4e8b6c`;
+      sidebar.style.borderRadius = `0 0 50% 0`;
+      sidebarButtonImg.src = "assets/icons/menu.svg";
+    } else if (sidebarOpen != null) {
+      sidebarROTC.style.display = `flex`;
+      sidebarOpen.id = "menu-button"; // a shorter version of document.getElementById("ID1-").id = "ID-2";
+      //sidebarClosed.style.display = `none`;
+      sidebarOpen.style.display = `inherit`;
+      sidebar.style.width = `200px`;
+      sidebar.style.padding = `20px 20px 10px 20px`;
+      sidebar.style.background = `#8ae0b4`;
+      sidebar.style.borderRadius = ``;
+      sidebarButtonImg.src = "assets/icons/menu-close.svg";
+    }
+  });
 });
-
-function sidebarOpenClose() {
-  let sidebarClosed = document.getElementById("menu-button");
-  let sidebarOpen = document.getElementById("back-icon");
-  const sidebarButtonImgSrc = document.getElementsByClassName("sidebar-buttonCLASS");
-
-  if (sidebarOpen != null) {
-    sidebar.sidebarROTC.style.display = `inherit`;
-    sidebarClosed.style.display = `none`;
-    sidebarOpen.style.display = `inherit`;
-    sidebar.style.width = `200px`;
-    sidebar.style.padding = `20px 20px 10px 20px`;
-    sidebar.style.background = `#8ae0b4`;
-    sidebar.style.borderRadius = ` `;
-    document.getElementById("menu-button").id = "back-icon";
-    sidebarButtonImgSrc.src = "assets/icons/menu-close.svg";
-  } else if (sidebarClosed != null) {
-    sidebar.sidebarROTC.style.display = `none`;
-    sidebarClosed.style.display = `inherit`;
-    sidebarOpen.style.display = `none`;
-    sidebar.style.width = `50px`;
-    sidebar.style.padding = `20px 20px 20px 20px`;
-    sidebar.style.background = `#4e8b6c`;
-    sidebar.style.borderRadius = `0 0 50% 0`;
-    document.getElementById("back-icon").id = "menu-button";
-    sidebarButtonImgSrc.src = "assets/icons/menu.svg";
-  }
-}

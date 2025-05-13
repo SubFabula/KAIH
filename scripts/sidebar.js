8497276469595340c08c386cfc67af1afb32e8e2
SubFabula <150776952+SubFabula@users.noreply.github.com>
2025-05-13 20:27:29 +0300

    Update29 Fix17

--- scripts/sidebar.js
+++ scripts/sidebar.js
@@ -1,33 +1,22 @@
 document.addEventListener("DOMContentLoaded", () => {
   const sidebar = document.querySelector(".sidebar");
-  const sidebarROTC = document.getElementById("rest-of-the-content"); // sidebarROTC => sidebarRestOfTheContent
-  const sidebarRealButton = document.getElementById("sidebarRealButton");
+  const sidebarButton = document.getElementById("sidebarRealButton");
   const sidebarButtonImg = document.querySelector(".sidebar-buttonCLASS"); // query selector if styling a class (something to do with «HTMLclass» and «single elements» stuff)
 
-  sidebarRealButton.addEventListener("click", () => {
-    const sidebarClosed = document.getElementById("menu-button");
-    const sidebarOpen = document.getElementById("back-icon");
+  sidebarButton.addEventListener("click", () => {
+    const isOpen = sidebar.classList.contains("open");
 
-    if (sidebarClosed != null) {
-      sidebarROTC.style.display = `none`;
-      sidebarClosed.id = "back-icon"; 
-      sidebarClosed.style.display = `inherit`;
-      //sidebarOpen.style.display = `none`;
-      sidebar.style.width = `50px`;
-      sidebar.style.padding = `20px 20px 20px 20px`;
-      sidebar.style.background = `#4e8b6c`;
-      sidebar.style.borderRadius = `0 0 50% 0`;
+    if (isOpen) {
+      sidebar.classList.remove("open");
+      sidebar.classList.add("closed");
       sidebarButtonImg.src = "assets/icons/menu.svg";
-    } else if (sidebarOpen != null) {
-      sidebarROTC.style.display = `flex`;
-      sidebarOpen.id = "menu-button"; // a shorter version of document.getElementById("ID1-").id = "ID-2";
+      sidebarButtonImg.id = "menu-button"; // a shorter version of document.getElementById("ID1-").id = "ID-2";
       //sidebarClosed.style.display = `none`;
-      sidebarOpen.style.display = `inherit`;
-      sidebar.style.width = `200px`;
-      sidebar.style.padding = `20px 20px 10px 20px`;
-      sidebar.style.background = `#8ae0b4`;
-      sidebar.style.borderRadius = ``;
+    } else {
+      sidebar.classList.remove("closed");
+      sidebar.classList.add("open");
       sidebarButtonImg.src = "assets/icons/menu-close.svg";
+      sidebarButtonImg.id = "back-icon";
     }
   });
 });

836853c815cda2a82695685c7f0ead34322175f5
SubFabula <150776952+SubFabula@users.noreply.github.com>
2025-07-04 21:41:43 +0300

    @media fix for "Notice.js" on mobile devices.

--- scripts/notice.js
+++ scripts/notice.js
@@ -7,20 +7,70 @@ document.addEventListener("DOMContentLoaded", () => {
   const noticeList = document.createElement("ul");
   const noticeConfirmButton = document.createElement("button");
 
-  noticeDiv.id = "noticeDiv"
-  noticeUI.id = "noticeUI"
-  noticeCloseIcon.id = "noticeCloseIcon"
-  noticeTitle.id = "noticeTitle"
-  noticeContext.id = "noticeContext"
+  noticeDiv.id = "noticeDiv";
+  noticeUI.id = "noticeUI";
+  noticeCloseIcon.id = "noticeCloseIcon";
+  noticeTitle.id = "noticeTitle";
+  noticeContext.id = "noticeContext";
   noticeList.id = "noticeList";
-  noticeConfirmButton.id = "noticeConfirmButton"
+  noticeConfirmButton.id = "noticeConfirmButton";
 
-  noticeConfirmButton.addEventListener("click", noticeClose)
+  noticeConfirmButton.addEventListener("click", noticeClose);
 
   document.body.append(noticeDiv);
   noticeDiv.append(noticeUI);
   noticeUI.append(noticeCloseIcon, noticeTitle, noticeContext, noticeList, noticeConfirmButton);
 
+  if (window.innerWidth <= 719) {
+    // @media (max-width: [Mobile])
+    // #noticeUI Styles
+    noticeUI.style.maxWidth = "70vw";
+    noticeUI.style.minWidth = "40vw";
+    noticeUI.style.maxHeight = "80vh";
+    noticeUI.style.minHeight = "60vh";
+    noticeUI.style.aspectRatio = "1/1.25";
+    noticeUI.style.padding = "2.5vw";
+
+    // #noticeTitle Styles
+    noticeTitle.style.fontSize = "5vw";
+    
+    // #noticeContext Styles
+    noticeContext.style.fontSize = "3.5vw";
+
+    // #noticeList Styles
+    noticeList.style.width = "50vw";
+    noticeList.style.fontSize = "3vw";
+    noticeList.style.paddingRight = "3vw";
+
+    // #noticeConfirmButton Styles
+    noticeConfirmButton.style.width = "50vw";
+    noticeConfirmButton.style.height = "10vh";
+    noticeConfirmButton.style.aspectRatio = "1/2";
+    noticeConfirmButton.style.fontSize = "3vw";
+  } else if (window.innerWidth >= 720) {
+    // @media (max-width: [Desktop])
+    // #noticeUI Styles
+    noticeUI.style.maxWidth = "40vw";
+    noticeUI.style.maxHeight = "65vh";
+    noticeUI.style.aspectRatio = "1/1.25";
+    noticeUI.style.padding = "2vw";
+
+    // #noticeTitle Styles
+    noticeTitle.style.fontSize = "2vw";
+    
+    // #noticeContext Styles
+    noticeContext.style.fontSize = "1.5vw";
+
+    // #noticeList Styles
+    noticeList.style.width = "20vw";
+    noticeList.style.fontSize = "1.25vw";
+
+    // #noticeConfirmButton Styles
+    noticeConfirmButton.style.width = "20vw";
+    noticeConfirmButton.style.height = "10vh";
+    noticeConfirmButton.style.aspectRatio = "1/2";
+    noticeConfirmButton.style.fontSize = "1vw";
+  }
   // #noticeDiv Styles
   noticeDiv.style.top = "0"
   noticeDiv.style.left = "0"
@@ -36,10 +86,7 @@ document.addEventListener("DOMContentLoaded", () => {
   noticeUI.style.top = "50%"
   noticeUI.style.left = "50%"
   noticeUI.style.transform = "translate(-50%, -50%)";
-  noticeUI.style.maxWidth = "40vw";
-  noticeUI.style.maxHeight = "65vh";
   noticeUI.style.aspectRatio = "1/1.25";
-  noticeUI.style.padding = "2vw";
   noticeUI.style.boxSizing = "border-box";
   noticeUI.style.position = "relative";
   noticeUI.style.display = "flex";
@@ -51,6 +98,7 @@ document.addEventListener("DOMContentLoaded", () => {
   noticeUI.style.overflow = "hidden";
   noticeUI.style.backgroundColor = "#ffffffd0";
   noticeUI.style.textAlign = "center";
+  noticeUI.style.textWrap = "pretty";
 
   // #noticeCloseIcon Styles & Scripts
   function noticeClose() {
@@ -62,32 +110,24 @@ document.addEventListener("DOMContentLoaded", () => {
     isEnglish
       ? "NOTICE"
       : "BİLGİLENDİRME";
-  noticeTitle.style.fontSize = "2vw";
 
   // #noticeContext Styles
   noticeContext.innerText =
     isEnglish
       ? "This WebSite is still under construction. Please be aware that there may be errors and unfinished content."
       : "Bu WebSitesi hâlen yapım aşamasındadır. Oluşabilecek hatalar ve eksik içerikler konusunda lütfen dikkatli olun.";
-  noticeContext.style.fontSize = "1.5vw";
 
   // #noticeList Styles
-  noticeList.style.width = "20vw";
   noticeList.innerHTML =
     isEnglish
     ? "<li>Calculation by Bill(₺) hasn't been setup yet. It will just factor the given data from 1 to 8.</li>"
     : "<li>Tutar/Fatura(₺) ile Hesaplama yöntemi henüz hazır değil. Girilen veriler sırasıyla 1'den 8 çarpılıyor.</li>";
-  noticeList.style.fontSize = "1.25vw";
 
   // #noticeConfirmButton Styles
-  noticeConfirmButton.style.width = "20vw";
-  noticeConfirmButton.style.height = "10vh";
-  noticeConfirmButton.style.aspectRatio = "1/2";
   noticeConfirmButton.innerText =
     isEnglish
       ? "Okay! Got it."
       : "Tamamdır! Anlaşıldı.";
-  noticeConfirmButton.style.fontSize = "1vw";
   noticeConfirmButton.disabled = "disabled";
   function noticeWaitForConfirmButton() {
     noticeConfirmButton.disabled = !noticeConfirmButton.disabled // "[thing].disabled = ![thing].disabled" enables the disabled [thing].
